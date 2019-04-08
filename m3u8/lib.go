5808a66765f8893ba204bd8469dcf051e8fd3e0b
Christoph Reif <christoph.reif@gmail.com>
2019-04-08 20:28:19 +0200

    Put some components to lib.go

--- m3u8/lib.go
+++ m3u8/lib.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"net/url"
 	"path"
+	"time"
 
 	"github.com/mysheep/cell/cm3u8"
+	"github.com/mysheep/cell/ctime"
+	"github.com/mysheep/cell/web"
 )
 
 func printMsg(object string, msg string) {
@@ -27,4 +30,51 @@ func makeAbsolute(base, url cm3u8.M3U8URL) cm3u8.M3U8URL {
 		return base + url
 	}
 	return url
+}
+
+func StartStopTimer(timeSlots <-chan TimeSlot, startSignals chan<- bool, stopSignals chan<- bool) {
+
+	starts := make(chan time.Time)
+	ends := make(chan time.Time)
+
+	go ctime.Timer(starts, startSignals)
+	go ctime.Timer(ends, stopSignals)
+
+	for {
+		ts := <-timeSlots
+		starts <- ts.start
+		ends <- ts.end
+	}
+
+}
+
+func Downloader(items <-chan DownloadItem, downloaded chan<- cm3u8.M3U8URL) {
+
+	urls := make(chan string)
+	contents := make(chan []byte)
+	filenames := make(chan string)
+	bytess := make(chan []byte)
+	savedFilenames := make(chan string)
+
+	go web.Downloader(urls, contents)
+	go web.Saver(filenames, bytess, savedFilenames)
+
+	for {
+		item := <-items
+
+		// Send url and ...
+		urls <- string(item.url)
+
+		// ... wait for downloaded content
+		content := <-contents
+
+		// Send filename and bytess and ...
+		fileName := item.folder + getFilename(item.url)
+		filenames <- fileName
+		bytess <- content
+
+		// ... wait for file is saved
+		<-savedFilenames
+		downloaded <- item.url
+	}
 }
--- m3u8/main.go
+++ m3u8/main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/grafov/m3u8"
 	"github.com/mysheep/cell"
 	"github.com/mysheep/cell/cm3u8"
-	"github.com/mysheep/cell/ctime"
-	"github.com/mysheep/cell/web"
 )
 
 const (
@@ -154,53 +152,6 @@ func Grapper(orders <-chan DownloadOrder) {
 		printMsg("Grapper", "STOP signal was fired. Set stop = true.")
 		stop = true
 	}()
-}
-
-func Downloader(items <-chan DownloadItem, downloaded chan<- cm3u8.M3U8URL) {
-
-	urls := make(chan string)
-	contents := make(chan []byte)
-	filenames := make(chan string)
-	bytess := make(chan []byte)
-	savedFilenames := make(chan string)
-
-	go web.Downloader(urls, contents)
-	go web.Saver(filenames, bytess, savedFilenames)
-
-	for {
-		item := <-items
-
-		// Send url and ...
-		urls <- string(item.url)
-
-		// ... wait for downloaded content
-		content := <-contents
-
-		// Send filename and bytess and ...
-		fileName := item.folder + getFilename(item.url)
-		filenames <- fileName
-		bytess <- content
-
-		// ... wait for file is saved
-		<-savedFilenames
-		downloaded <- item.url
-	}
-}
-
-func StartStopTimer(timeSlots <-chan TimeSlot, startSignals chan<- bool, stopSignals chan<- bool) {
-
-	starts := make(chan time.Time)
-	ends := make(chan time.Time)
-
-	go ctime.Timer(starts, startSignals)
-	go ctime.Timer(ends, stopSignals)
-
-	for {
-		ts := <-timeSlots
-		starts <- ts.start
-		ends <- ts.end
-	}
-
 }
 
 func main() {
