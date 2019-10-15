999b002e3bb25f464268586c44bb1a1153a9cbc6
Christoph Reif <christoph.reif@gmail.com>
2019-10-15 08:02:45 +0200

    refactor move interface to brain.go file

--- Cell/vendor/github.com/mysheep/cell/brain/brain.go
+++ Cell/vendor/github.com/mysheep/cell/brain/brain.go
@@ -7,7 +7,34 @@ import (
 	"time"
 )
 
-// TODO: see integer.Aggregate to work with a list of input channels
+// ----------------------------------------------------------------------------
+// Connect interface between cells
+// ----------------------------------------------------------------------------
+
+type InputConnector interface {
+	InputConnect(ch chan int, weight float64)
+}
+
+type OutputConnector interface {
+	OutputConnect(ch chan int)
+}
+
+type Namer interface {
+	Name() string
+}
+
+func ConnectBy(out OutputConnector, in InputConnector, weight float64) {
+
+	connection := make(chan int)
+	out.OutputConnect(connection)
+	in.InputConnect(connection, weight)
+
+	//fmt.Println(fmt.Sprintf("cell '%s' connected to '%s'", out.(Namer).Name(), in.(Namer).Name()))
+}
+
+// ----------------------------------------------------------------------------
+// Cell parts
+// ----------------------------------------------------------------------------
 
 func Synapse(weight float64, in <-chan int, out chan<- float64) func() {
 
